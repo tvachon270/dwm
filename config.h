2dfc6e4742189b12e8a5e852289e955df07d26d8
tvachonSDB <t.vachon@softdb.com>
2021-07-26 14:17:08 -0400

    Changed colors, added media keys to control spotify and added a printscreen button

--- config.h
+++ config.h
@@ -6,9 +6,12 @@
 #define XK_VOLM XF86XK_AudioMute
 #define XK_VOLU XF86XK_AudioRaiseVolume
 #define XK_VOLD XF86XK_AudioLowerVolume
+#define XK_PLAY XF86XK_AudioPlay
+#define XK_PREV XF86XK_AudioPrev
+#define XK_NEXT XF86XK_AudioNext
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 30;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
@@ -19,15 +22,30 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:pixelsize=15:antialias=true:autohint=false" };
 static const char dmenufont[]       = "Calling Code:pixelsize=15:antialias=true:autohint=false";
+static const char dmenulines[]      = "20";
+
+/* Basic dwm colors */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+/* My colors */
+static const char col_purple[]      = "#ba34eb";
+static const char col_dark_purple[] = "#2f0f8c";
+static const char col_dark_violet[] = "#6a58a1";
+static const char col_pink[]        = "#eb34d5";
+static const char col_blue[]        = "#344ceb";
+static const char col_white[]       = "#ffffff";
+
+/* Basic scheme */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
+/*	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },*/
+	/* My scheme */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_dark_purple,  col_dark_purple  },
 };
 
 /* tagging */
@@ -81,12 +99,17 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", dmenulines, "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_dark_purple, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+
+#define scrotcmd "scrot --delay 1 -s -z '%Y-%m-%d-%H%M%S_$wx$h_scrot.png' -e 'mv $f ~/Downloads/scrot/'"
 
 #define volmutecmd "pactl set-sink-mute @DEFAULT_SINK@ toggle"
 #define voldowncmd "pactl set-sink-volume @DEFAULT_SINK@ -2%"
 #define volupcmd "pactl set-sink-volume @DEFAULT_SINK@ +2%"
+#define playcmd "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"
+#define nextcmd "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"
+#define prevcmd "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous"
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -95,6 +118,10 @@ static Key keys[] = {
 	{ 0,                            XK_VOLM,   spawn,          SHCMD(volmutecmd) },
 	{ 0,                            XK_VOLD,   spawn,          SHCMD(voldowncmd) },
 	{ 0,                            XK_VOLU,   spawn,          SHCMD(volupcmd) },
+	{ 0,                            XK_PLAY,   spawn,          SHCMD(playcmd) },
+	{ 0,                            XK_PREV,   spawn,          SHCMD(prevcmd) },
+	{ 0,                            XK_NEXT,   spawn,          SHCMD(nextcmd) },
+	{ 0,				XK_Print,  spawn, 	   SHCMD(scrotcmd) },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -133,8 +160,8 @@ static Key keys[] = {
 	/* Main gaps modifiers */
 	{ MODKEY,              		XK_equal,  incrgaps,       {.i = +5 } },
 	{ MODKEY,			XK_minus,  incrgaps,       {.i = -5 } },
-	{ MODKEY,			XK_0,      defaultgaps,    {0} }, 
-	{ MODKEY|ShiftMask,	        XK_0,      togglegaps,     {0} },
+	{ MODKEY|ShiftMask,		XK_equal,  defaultgaps,    {0} }, 
+	{ MODKEY|ShiftMask,	        XK_minus,  togglegaps,     {0} },
 	/* Other gaps modifiers */
 	{ MODKEY|Mod1Mask,              XK_i,      incrigaps,      {.i = +5 } },
         { MODKEY|Mod1Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -5 } },
